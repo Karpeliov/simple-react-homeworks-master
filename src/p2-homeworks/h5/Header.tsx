950c54cd8d7859af461566580562061dacf56d32
Karpeliov <alex.karpelev@gmail.com>
2020-12-14 16:54:50 +0300

    The 3rd commit

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
 
 function Header() {
     return (
         <div>
             // add NavLinks
+            <NavLink to="Junior" >Junior</NavLink>
+            {/*<NavLink to="/Junior+" >Junior+</NavLink>*/}
 
         </div>
     );
