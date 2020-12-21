395cf2af8ff815852b8b94c60adceb50713d988c
Karpeliov <alex.karpelev@gmail.com>
2020-12-21 12:39:29 +0300

    Updates

---
+++ precache-manifest.7f65c126f1b2895a7680ea2a97056aaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b779b85bd1b31812acd8",
+    "url": "/simple-react-homeworks-master/static/css/main.4ab18628.chunk.css"
+  },
+  {
+    "revision": "b779b85bd1b31812acd8",
+    "url": "/simple-react-homeworks-master/static/js/main.7b6f5366.chunk.js"
+  },
+  {
+    "revision": "934caa94c485bae7831b",
+    "url": "/simple-react-homeworks-master/static/js/runtime~main.0ffd70ec.js"
+  },
+  {
+    "revision": "7a65bebe1bdcb7b2e20e",
+    "url": "/simple-react-homeworks-master/static/js/2.f8c8ddb9.chunk.js"
+  },
+  {
+    "revision": "233620a0872191d4aad276ac62be0358",
+    "url": "/simple-react-homeworks-master/index.html"
+  }
+];
