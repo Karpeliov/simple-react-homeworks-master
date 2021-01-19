377109cb4c7bac64db452eef80282a62f73758d2
alexk <alex.karpelev@gmail.com>
2021-01-19 23:10:27 +0300

    Commit HW10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useReducer} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import {InitialStateType, loadingReducer, loadingAC} from "./bll/loadingReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false;
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.loading)
+    const dispatch = useDispatch()
 
     const setLoading = () => {
         // dispatch
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 2000)
+
         console.log("loading...");
     };
 
@@ -19,9 +27,12 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div style={{textAlign: "center"}}>
+                        <img
+                            src={"https://mir-s3-cdn-cf.behance.net/project_modules/disp/f1055231234507.564a1d234bfb6.gif"}/>
+                    </div>
                 ) : (
-                    <div>
+                    <div style={{textAlign: "center"}}>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,26 @@
-const initState = {
+type LoadingActionType = {
+    type: "LOADING"
+    loading: boolean
+}
+export type InitialStateType = typeof initState
 
+const initState = {
+    loading: false
 };
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): InitialStateType => {
     switch (action.type) {
-        case "": {
-            return state;
+        case "LOADING": {
+            return {...state, loading: action.loading};
         }
-        default: return state;
+        default:
+            return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (loading: boolean): LoadingActionType => {
+    return {
+        type: "LOADING",
+        loading
+    }
+};
